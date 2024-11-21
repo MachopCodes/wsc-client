3493cc6479df19f4f3c6015758372e17ce1f83fb
Will Andreae <wcandreae@gmail.com>
2024-11-21 12:42:37 -0500

    prep for gh pages deploy

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,12 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  output: "export", // Enable static export
+  trailingSlash: true, // Ensure trailing slashes for all paths
+  images: {
+    unoptimized: true, // Disable image optimization (GitHub Pages doesn't support this)
+  },
+  basePath: "https://github.com/MachopCodes/wsc-client.git", 
 };
 
 export default nextConfig;
