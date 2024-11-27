b6a93b292989569fb7b9db623e8e011556cc0bbb
Will Andreae <wcandreae@gmail.com>
2024-11-27 10:45:28 -0500

    add more server logs

--- src/utils/db.ts
+++ src/utils/db.ts
@@ -1,6 +1,13 @@
 import mysql, { Connection } from "mysql2/promise";
 
 export const createConnection = async (): Promise<Connection> => {
+  console.log(
+    process.env.DB_HOST,
+    process.env.DB_USER,
+    Number(process.env.DB_PORT),
+    process.env.DB_PASSWORD,
+    process.env.DB_NAME
+  );
   return mysql.createConnection({
     host: process.env.DB_HOST,
     user: process.env.DB_USER,
