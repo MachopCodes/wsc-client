95737495c1fac6123ed745a1d1a7d19ba4b4876e
Will Andreae <wcandreae@gmail.com>
2024-12-05 09:54:42 -0500

    styling and rm price for now

--- src/components/ProductCard.tsx
+++ src/components/ProductCard.tsx
@@ -2,6 +2,7 @@ import { ProductCardProps } from "@/interfaces/Product";
 import Image from "next/image";
 
 export default function ProductCard({ product }: ProductCardProps) {
+  console.log('product', product)
   return (
     <div className="border border-gray-300 rounded-lg shadow-md bg-white bg-opacity-80 w-full overflow-hidden grid grid-cols-2">
       {/* Left Column: Image */}
@@ -25,63 +26,63 @@ export default function ProductCard({ product }: ProductCardProps) {
       {/* Right Column: Product Details */}
       <div className="p-8 text-left">
         <h2 className="text-xl font-bold text-gray-800">{product.name}</h2>
-        {product.year && (
-          <p className="text-sm text-gray-600">Year: {product.year}</p>
+        {product.year !== 0 && (
+          <p className="text-sm text-gray-600"><b>Year:</b> {product.year}</p>
         )}
         {product.region && (
-          <p className="text-sm text-gray-600">Region: {product.region}</p>
+          <p className="text-sm text-gray-600"><b>Region:</b> {product.region}</p>
         )}
         {product.type && (
-          <p className="text-sm text-gray-600">Type: {product.type}</p>
+          <p className="text-sm text-gray-600"><b>Type:</b> {product.type}</p>
         )}
-        {product.price !== undefined && (
+        {/* {product.price !== undefined && (
           <p className="text-lg font-semibold text-gray-800 mt-2">
             ${Number(product.price).toFixed(2)}
           </p>
-        )}
+        )} */}
         {product.serving_temperature && (
           <p className="text-sm text-gray-600">
-            Serving Temperature: {product.serving_temperature}°f
+            <b>Serving Temperature:</b> {product.serving_temperature}°f
           </p>
         )}
         {product.food_pairings && (
           <p className="text-sm text-gray-600">
-            Food Pairings: {product.food_pairings}
+            <b>Food Pairings:</b> {product.food_pairings}
           </p>
         )}
         {product.grape_varietals && (
           <p className="text-sm text-gray-600">
-            Grape Varietals: {product.grape_varietals}
+            <b>Grape Varietals:</b> {product.grape_varietals}
           </p>
         )}
         {product.ageing_process && (
           <p className="text-sm text-gray-600">
-            Ageing Process: {product.ageing_process}
+            <b>Ageing Process:</b> {product.ageing_process}
           </p>
         )}
         {product.tasting_notes && (
           <p className="text-sm text-gray-600">
-            Tasting Notes: {product.tasting_notes}
+            <b>Tasting Notes:</b> {product.tasting_notes}
           </p>
         )}
         {product.location_notes && (
           <p className="text-sm text-gray-600">
-            Location Notes: {product.location_notes}
+            <b>Location Notes:</b> {product.location_notes}
           </p>
         )}
         {product.brand_description && (
           <p className="text-sm text-gray-600">
-            Brand Description: {product.brand_description}
+            <b>Brand Description:</b> {product.brand_description}
           </p>
         )}
         {product.winemaker_notes && (
           <p className="text-sm text-gray-600">
-            Winemaker Notes: {product.winemaker_notes}
+            <b>Winemaker Notes:</b> {product.winemaker_notes}
           </p>
         )}
         {product.harvesting_technique && (
           <p className="text-sm text-gray-600">
-            Harvesting Technique: {product.harvesting_technique}
+            <b>Harvesting Technique:</b> {product.harvesting_technique}
           </p>
         )}
         <p className="text-gray-700 mt-4 line-clamp-3">{product.description}</p>
